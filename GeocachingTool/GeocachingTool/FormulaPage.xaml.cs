8aadaf8f361cfd5e992a4c60ecb82ba75da38629
Finn Alberts <2062662alberts@zuyd.nl>
2021-02-22 17:24:18 +0100

    Implemented #8

--- GeocachingTool/GeocachingTool/FormulaPage.xaml.cs
+++ GeocachingTool/GeocachingTool/FormulaPage.xaml.cs
@@ -61,7 +61,7 @@ namespace GeocachingTool
             }
             else
             {
-                string result = SolveCoordinates(formula);
+                string result = Calculate(formula);
 
                 answerLabel.Text = result;
             }
@@ -95,40 +95,6 @@ namespace GeocachingTool
             }
         }
 
-        private string SolveCoordinates(string coordinates)
-        {
-            string solvedCoordinates = String.Empty;
-            int beginOfEquation = 0;
-            int endOfEquation = 0;
-            
-            int openedBrackets = 0;
-            for (int i = 0; i < coordinates.Length; i++)
-            {
-                if (coordinates[i] == '(')
-                {
-                    openedBrackets += 1;
-                    if (openedBrackets == 1)
-                    {
-                        beginOfEquation = i;
-                    }
-                } 
-                else if (coordinates[i] == ')')
-                {
-                    openedBrackets -= 1;
-                    if (openedBrackets == 0)
-                    {
-                        endOfEquation = i;
-                        solvedCoordinates += Calculate(coordinates.Substring(beginOfEquation, endOfEquation - beginOfEquation + 1));
-                    }
-                } else if (openedBrackets == 0)
-                {
-                    solvedCoordinates += coordinates[i];
-                }
-            }
-
-            return solvedCoordinates;
-        }
-
         private async void deleteAllToolbarItem_Clicked(object sender, EventArgs e)
         {
             bool answer = await DisplayAlert("Waarschuwing", "Weet je zeker dat je alle opgeslagen letters inclusief waarde wilt verwijderen? Dit kan niet ongedaan worden gemaakt.", "Ja", "Nee");
@@ -150,7 +116,7 @@ namespace GeocachingTool
 
         private void helpToolbarItem_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Help", "Voeg letters toe door op 'Nieuw' te tikken. Geef voor iedere letter aan wat de waarde van deze letter is. De letter zal nu in het overzicht verschijnen. Vanaf hier kun je letter ook verwijderen of aanpassen. \n\nDoor bovenin een coördinaat met formule(s) in te voeren, zal de app de coördinaten voor je uitrekenen. In de formule mogen de leters blijven staan. \n\nVoorbeeld: je hebt de letters a=3 en b=12 ingevoerd. Je hebt als formule N 51 20.(a*100) E 006 07.(b*10+17). De app geeft nu als antwoord N 51 20.300 E 006 07.137.", "Oke");
+            DisplayAlert("Help", "Voeg letters toe door op 'Nieuw' te tikken. Geef voor iedere letter aan wat de waarde van deze letter is. De letter zal nu in het overzicht verschijnen. Vanaf hier kun je letter ook verwijderen of aanpassen. \n\nBovenin kun je een formule invoeren om een waarde uit te rekenen. Deze kunnen simpel zijn zoals 5+5, maar deze mogen ook letters bevatten die je hebt toegevoegd. Je kunt gebruik maken van +, -, * en /.\n\nVoorbeeld: je hebt de letter 'a' toegevoegd met als waarde 4. Door 10-a in te voeren zul je als antwoord 6 krijgen.", "Oke");
         }
     }
 }
