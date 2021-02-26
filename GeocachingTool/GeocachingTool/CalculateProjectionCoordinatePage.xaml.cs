a92c55152b4de783a4a5f7f6c3f9babf170d9176
Finn Alberts <2062662alberts@zuyd.nl>
2021-02-26 15:29:43 +0100

    Implemented #3

---
+++ GeocachingTool/GeocachingTool/CalculateProjectionCoordinatePage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GeocachingTool
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalculateProjectionCoordinatePage : ContentPage
+    {
+        public CalculateProjectionCoordinatePage()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Clicked(object sender, EventArgs e)
+        {
+            // Get input
+            string northDegreesEntry = northCoordinateEntry.Text;
+            string northMinutesEntry = northMinuteEntry.Text;
+            string eastDegreesEntry = eastCoordinateEntry.Text;
+            string eastMinutesEntry = eastMinuteEntry.Text;
+            string bearingEntry = angleEntry.Text;
+            string meteresEntry = distanceEntry.Text;
+
+            // Check if filled in
+            if (String.IsNullOrEmpty(northDegreesEntry) || String.IsNullOrEmpty(northMinutesEntry) || String.IsNullOrEmpty(eastDegreesEntry) || String.IsNullOrEmpty(eastMinutesEntry) || String.IsNullOrEmpty(bearingEntry) || String.IsNullOrEmpty(meteresEntry))
+            {
+                DisplayAlert("Fout", "Niet alle velden zijn ingevuld. Vul alle velden in en probeer het opnieuw.", "Oke");
+            }
+            else
+            {
+                // Convert to floats
+                float northDegrees = float.Parse(northDegreesEntry.Replace(",", "."), System.Globalization.CultureInfo.InvariantCulture);
+                float northMinutes = float.Parse(northMinutesEntry.Replace(",", "."), System.Globalization.CultureInfo.InvariantCulture);
+                float eastDegrees = float.Parse(eastDegreesEntry.Replace(",", "."), System.Globalization.CultureInfo.InvariantCulture);
+                float eastMinutes = float.Parse(eastMinutesEntry.Replace(",", "."), System.Globalization.CultureInfo.InvariantCulture);
+                float bearing = float.Parse(bearingEntry.Replace(",", "."), System.Globalization.CultureInfo.InvariantCulture);
+                float distance = float.Parse(meteresEntry.Replace(",", "."), System.Globalization.CultureInfo.InvariantCulture) / 1000;
+
+                // Convert
+                float north = northDegrees + northMinutes / 60;
+                float east = eastDegrees + eastMinutes / 60;
+
+                double earthRadius = 6371.01;
+                var distRatio = distance/ earthRadius;
+                var distRatioSine = Math.Sin(distRatio);
+                var distRatioCosine = Math.Cos(distRatio);
+
+                var startLatRad = DegreesToRadians(north);
+                var startLonRad = DegreesToRadians(east);
+
+                var startLatCos = Math.Cos(startLatRad);
+                var startLatSin = Math.Sin(startLatRad);
+
+                var endLatRads = Math.Asin((startLatSin * distRatioCosine) + (startLatCos * distRatioSine * Math.Cos(DegreesToRadians(bearing))));
+
+                var endLonRads = startLonRad + Math.Atan2(Math.Sin(DegreesToRadians(bearing)) * distRatioSine * startLatCos, distRatioCosine - startLatSin * Math.Sin(endLatRads));
+
+                var endNorth = RadiansToDegrees(endLatRads);
+                var endEast = RadiansToDegrees(endLonRads);
+
+                var northDegreesAnswer = (int)Math.Floor(endNorth);
+                var northMinutesAnswer = (endNorth - (float)Math.Floor(endNorth)) * 60;
+
+                var eastDegreesAnswer = (int)Math.Floor(endEast);
+                var eastMinutesAnswer = (endEast - (float)Math.Floor(endEast)) * 60;
+
+                answerLabel.Text = String.Format("N{0}° {1:f3} E{2}° {3:f3}", northDegreesAnswer, northMinutesAnswer, eastDegreesAnswer, eastMinutesAnswer);
+            }
+        }
+
+        public static double DegreesToRadians(double degrees)
+        {
+            const double degToRadFactor = Math.PI / 180;
+            return degrees * degToRadFactor;
+        }
+
+        public static double RadiansToDegrees(double radians)
+        {
+            const double radToDegFactor = 180 / Math.PI;
+            return radians * radToDegFactor;
+        }
+    }
+}
